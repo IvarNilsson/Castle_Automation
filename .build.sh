0b3e01af5025fee119f49e7ed7302c82138833a1
Ivar Nilsson <ivar.j.nilsson@gmail.com>
2023-09-24 17:19:37 +0200

    works ok but still some buggs with PCB

--- .build.sh
+++ .build.sh
@@ -3,8 +3,8 @@ echo begin compile and flash $1
 mkdir run
 mkdir run/$1
 
-sudo cp src/$1.cpp run/$1
-sudo mv run/$1/$1.cpp run/$1/$1.ino
+cp src/$1.cpp run/$1
+mv run/$1/$1.cpp run/$1/$1.ino
 
 arduino-cli compile --fqbn esp32:esp32:nodemcu-32s run/$1/$1.ino
 arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:nodemcu-32s run/$1/$1.ino
--- src/test_pins.cpp
+++ src/test_pins.cpp
@@ -4,10 +4,10 @@
 
 #define BUZZER 33
 
-#define BUTTN_R 36
-#define BUTTN_G 39
-#define BUTTN_B 34
-#define BUTTN_Y 32
+#define BTN_R 36
+#define BTN_G 39
+#define BTN_B 34
+#define BTN_Y 32
 
 #define RFID_RST 0
 #define RFID_MISO 19
@@ -23,19 +23,59 @@
 #define RGB_B 12  // might be wrong order :)
 
 #define LED_R 25
-#define LED_Y 26
-#define LED_G 27
+#define LED_Y 27
+#define LED_G 26
 
 #define FOTO_RESISTOR 13
 #define TEMP 35
 
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
 
-void setup() { pinMode(LED_R, OUTPUT); }
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-void loop() {
-    delay(500);
-    digitalWrite(LED_R, HIGH);
-    delay(500);
-    digitalWrite(LED_R, LOW);
+#define Serial Serial
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, -1);
+
+void setup() {
+    // pinMode(LED_R, OUTPUT);
+    // pinMode(LED_Y, OUTPUT);
+    // pinMode(LED_G, OUTPUT);
+
+    // pinMode(BTN_R, INPUT);
+    // pinMode(BTN_G, INPUT);
+    // pinMode(BTN_B, INPUT);
+    // pinMode(BTN_Y, INPUT);
+
+    pinMode(MATRIX_DATA, OUTPUT);
+    pinMode(MATRIX_CLOCK, OUTPUT);
+    pinMode(MATRIX_LATCH, OUTPUT);
+
+    digitalWrite(MATRIX_DATA, LOW);
+    digitalWrite(MATRIX_CLOCK, LOW);
+    digitalWrite(MATRIX_LATCH, HIGH);
+
+    Serial.begin(115200);
+    Serial.println("GO!");
+
+    if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+        Serial.println(F("SSD1306 allocation failed"));
+    }
+    delay(2000);
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(WHITE);
+    display.setCursor(0, 10);
+    // Display static text
+    display.println("Hello, world!");
+    display.display();
 }
+
+void loop() {}
