498305e25e92c2c930cd5c488c7e981fc94435d3
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-11-24 15:54:01 +0100

    now syntax and imports of libs finaly :-/

--- src/RTOS_test.cpp
+++ src/RTOS_test.cpp
@@ -27,9 +27,9 @@ extern "C" void app_main();
 #define OLED_SCL 22
 #define OLED_SDA 21
 
-#define RGB_R 15  // might be wrong order :)
-#define RGB_G 14  // might be wrong order :)
-#define RGB_B 12  // might be wrong order :)
+#define RGB_R 15 // might be wrong order :)
+#define RGB_G 14 // might be wrong order :)
+#define RGB_B 12 // might be wrong order :)
 
 #define LED_R GPIO_NUM_25
 #define LED_Y GPIO_NUM_26
@@ -38,49 +38,49 @@ extern "C" void app_main();
 #define FOTO_RESISTOR 13
 #define TEMP_SENSOR 35
 
-void task1(void *arg) {
-    gpio_pad_select_gpio(LED_R);
-    gpio_set_direction(LED_R, GPIO_MODE_OUTPUT);
-    while (1) {
-        printf("Turning off the LED\n");
-        gpio_set_level(LED_R, 0);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        printf("Turning on the LED\n");
-        gpio_set_level(LED_R, 1);
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
+void task_LED_R(void *arg) {
+  gpio_pad_select_gpio(LED_R);
+  gpio_set_direction(LED_R, GPIO_MODE_OUTPUT);
+  while (1) {
+    printf("Turning off the LED\n");
+    gpio_set_level(LED_R, 0);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    printf("Turning on the LED\n");
+    gpio_set_level(LED_R, 1);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
 }
 
-void task2(void *arg) {
-    gpio_pad_select_gpio(LED_Y);
-    gpio_set_direction(LED_Y, GPIO_MODE_OUTPUT);
-    while (1) {
-        printf("Turning off the LED\n");
-        gpio_set_level(LED_Y, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        printf("Turning on the LED\n");
-        gpio_set_level(LED_Y, 1);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
+void task_LED_Y(void *arg) {
+  gpio_pad_select_gpio(LED_Y);
+  gpio_set_direction(LED_Y, GPIO_MODE_OUTPUT);
+  while (1) {
+    printf("Turning off the LED\n");
+    gpio_set_level(LED_Y, 0);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    printf("Turning on the LED\n");
+    gpio_set_level(LED_Y, 1);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
 }
 
-void task3(void *arg) {
-    gpio_pad_select_gpio(LED_G);
-    gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
-    while (1) {
-        printf("Turning off the LED\n");
-        gpio_set_level(LED_G, 0);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-        printf("Turning on the LED\n");
-        gpio_set_level(LED_G, 1);
-        vTaskDelay(2000 / portTICK_PERIOD_MS);
-    }
+void task_LED_G(void *arg) {
+  gpio_pad_select_gpio(LED_G);
+  gpio_set_direction(LED_G, GPIO_MODE_OUTPUT);
+  while (1) {
+    printf("Turning off the LED\n");
+    gpio_set_level(LED_G, 0);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+    printf("Turning on the LED\n");
+    gpio_set_level(LED_G, 1);
+    vTaskDelay(2000 / portTICK_PERIOD_MS);
+  }
 }
 
 void app_main(void) {
-    // xTaskCreate(task1, "task1", 4096, NULL, 10, &myTask1Handle);
-    // xTaskCreate(task2, "task2", 4096, NULL, 9, &myTask2Handle);
-    xTaskCreate(task1, "task1", 4096, NULL, 10, NULL);
-    xTaskCreate(task2, "task2", 4096, NULL, 9, NULL);
-    xTaskCreate(task3, "task3", 4096, NULL, 8, NULL);
+  // xTaskCreate(task1, "task1", 4096, NULL, 10, &myTask1Handle);
+  // xTaskCreate(task2, "task2", 4096, NULL, 9, &myTask2Handle);
+  xTaskCreate(task_LED_R, "task1", 4096, NULL, 10, NULL);
+  xTaskCreate(task_LED_Y, "task2", 4096, NULL, 9, NULL);
+  xTaskCreate(task_LED_G, "task3", 4096, NULL, 8, NULL);
 }
