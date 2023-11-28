f6be63e5513cd9cb89785e104be3df4a0006ee9f
IvarNilsson <ivar.j.nilsson@gmail.com>
2023-11-28 12:53:08 +0100

    script improvmnets

--- .build.sh
+++ .build.sh
@@ -6,6 +6,12 @@ echo #new line
 echo "${BOLD}--- Begin compile and flash ${UNDERLINE}$1${RESET}"
 echo #new line
 
+# remove old dir if cancled before rm last time
+if [ -d "run" ]; then
+    rm -rf run/*
+    rmdir run
+fi
+
 # make temp dir for project to be run
 mkdir run
 mkdir run/$1
@@ -14,14 +20,17 @@ mkdir run/$1
 cp src/$1.cpp run/$1
 mv run/$1/$1.cpp run/$1/$1.ino
 
-cp src/$2.cpp run/$1
+# add second file if exists
+if ! [ -z "$2" ]; then
+    cp src/$2.cpp run/$1
+fi
+
 #mv run/$1/$2.cpp run/$1/$2.ino
 
 #arduino-cli compile --fqbn esp32:esp32:nodemcu-32s run/$1/$1.ino
 #arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:nodemcu-32s run/$1/$1.ino
 arduino-cli compile --fqbn esp32:esp32:esp32wrover run/$1/$1.ino --build-property build.partitions=min_spiffs --build-property upload.maximum_size=1966080
-arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32wrover run/$1/$1.ino 
-
+arduino-cli upload -p /dev/ttyUSB0 --fqbn esp32:esp32:esp32wrover run/$1/$1.ino
 
 rm -rf run/*
 rmdir run
